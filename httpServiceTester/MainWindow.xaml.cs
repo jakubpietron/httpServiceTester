9f81765526c1c87b52aa21dad991a32fb0d97682
Jakub Pietron <jakubpietron@gmail.com>
2018-09-29 14:46:56 +0200

    Added basic get functionality

--- httpServiceTester/MainWindow.xaml.cs
+++ httpServiceTester/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,43 @@ namespace httpServiceTester
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        static HttpClient client = new HttpClient();
+
+        private void cleartxtbox(object sender, RoutedEventArgs e)
+        {
+            if ((responseTextBox.Text == "Response") & (responseTextBox.IsFocused)) responseTextBox.Text = "";
+            if ((contentTextBox.Text == "Content") & (contentTextBox.IsFocused)) contentTextBox.Text = "";
+            if ((addressTextBox.Text == "http address") & (addressTextBox.IsFocused)) addressTextBox.Text = "";
+        }
+
+        private void buttonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            responseWebBrowser.NavigateToString(" ");
+
+            try
+            {
+                WebRequest request = WebRequest.Create(addressTextBox.Text.ToString());
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                responseTextBox.Text = "";
+                responseTextBox.Text = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+            }
+            catch (Exception)
+            {
+                responseTextBox.Text = "Request returned an error";
+            }
+
+            responseWebBrowser.NavigateToString(responseTextBox.Text.ToString());
+            
+
+
+
         }
     }
 }
